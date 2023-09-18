71cee7623cf7c8c2376e55af3b86a013430534c8
NataliaNikitina <nne2418@gmail.com>
2023-09-18 19:29:18 +0300

    Initial Commit

---
+++ BarCodeReader/BarCodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeReaderApp.swift
+//  BarCodeReader
+//
+//  Created by N N on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
